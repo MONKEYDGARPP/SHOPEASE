e56029e6df41f56a601206a36fe470c2009401f6
sonai99 <anirbandutta428@gmail.com>
2023-08-02 17:30:52 +0530

    Added Login/Signup context+reducer+buttons

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,36 +1,50 @@
 import { createContext, useContext, useReducer } from "react";
-import { cartReducer } from './Reducers';
-import { productReducer } from './Reducers';
-import productsData from '../db/data'
+import { cartReducer } from "./Reducers";
+import { productReducer } from "./Reducers";
+import { authReducer } from "./Reducers";
+import productsData from "../db/data";
 
 const Cart = createContext();
 
-const Context = ({children}) => {      
-    const products = productsData;
+const Context = ({ children }) => {
+  const products = productsData;
 
-      const [state, dispatch] = useReducer(cartReducer,{
-        products:products,
-        cart:[],
-      })
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
 
-      const[productState, productDispatch] = useReducer(productReducer,{
-        byStock:false,
-        byFastDelivery:false,
-        byRating:0,
-        searchQuery:""
-      })
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
 
-      console.log(productState)
+  const [loggedin, loginDispatch] = useReducer(authReducer, {
+    isLoggedIn: false,
+  });
 
-    return (
-        <Cart.Provider value={{ state,dispatch,productState,productDispatch }}>
-            {children}
-        </Cart.Provider>
-    )
-}
+  console.log(productState);
+
+  return (
+    <Cart.Provider
+      value={{
+        state,
+        dispatch,
+        productState,
+        productDispatch,
+        loggedin,
+        loginDispatch,
+      }}
+    >
+      {children}
+    </Cart.Provider>
+  );
+};
 
 export const CartState = () => {
-    return useContext(Cart);
-}
+  return useContext(Cart);
+};
 
 export default Context;
